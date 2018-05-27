94092437e8695dd2385ffa74bfbcfaa99f0ea905
Bob Hiltner <bobhilt@gmail.com>
2018-05-27 10:31:15 -1000

    Implement challenges, (block, additional count vars.)

--- catalog/views.py
+++ catalog/views.py
@@ -11,12 +11,19 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact=STATUS_AVAILABLE).count()
     num_authors = Author.objects.all().count()
-    
+    num_genres = Genre.objects.all().count()
+    num_warrior_books = Book.objects.filter(summary__icontains='warrior').count()
+    num_in_books = Book.objects.filter(title__icontains='in').count()
+
     return render(
         request,
         'index.html',
         context={'num_books' : num_books, 
                 'num_instances' : num_instances,
                 'num_instances_available': num_instances_available,
-                'num_authors' : num_authors},
+                'num_genres' : num_genres,
+                'num_authors' : num_authors,
+                'num_warrior_books' : num_warrior_books,
+                'num_in_books' : num_in_books,
+                },
     )
