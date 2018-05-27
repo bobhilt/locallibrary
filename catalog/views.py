ac7eb607022914a6bc4b2d6fe087a6119c96a89a
Bob Hiltner <bobhilt@gmail.com>
2018-05-26 20:28:19 -1000

    Added Home Page, bootstrap, css.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """
+    View function for home page of site.
+    """
+    STATUS_AVAILABLE = 'a'
+    
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=STATUS_AVAILABLE).count()
+    num_authors = Author.objects.all().count()
+    
+    return render(
+        request,
+        'index.html',
+        context={'num_books' : num_books, 
+                'num_instances' : num_instances,
+                'num_instances_available': num_instances_available,
+                'num_authors' : num_authors},
+    )
